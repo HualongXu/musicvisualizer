cd9d9ae2cafe2b2f2df7d7146121f8421276d4f7
Hualong Xu <hxu@smith.edu>
2023-12-11 03:54:15 -0500

    fixed some package importing issues

--- R/musicvisualizer.R
+++ R/musicvisualizer.R
@@ -20,13 +20,27 @@
 #' * album_name
 #'
 #' Note
-
+#'
+#' @importFrom spotifyr get_spotify_access_token
+#' @importFrom spotifyr get_artist_audio_features
+#' @importFrom dplyr filter
+#' @importFrom dplyr select
+#'
+#' @export
 musicvisualizer <- function(album_name, artist_name) {
 
-  x <- get_artist_audio_features(artist_name) |>
-    filter(album_name == album_name) |>
-    select(artist_name, album_images, album_release_date, danceability, energy, valence,
-           instrumentalness, acousticness, track_name, album_name)
+  #I am not sure if this trunk of code is supposed to be here in this R file -
+  id <- 'd0f427dc24ea4235a017135aaa1cd5ba'
+  secret <- '4866b1333932467cb761e1abd0d669bf'
+  Sys.setenv(SPOTIFY_CLIENT_ID = id)
+  Sys.setenv(SPOTIFY_CLIENT_SECRET = secret)
+  access_token <- spotifyr::get_spotify_access_token()
+
+
+  x <- spotifyr::get_artist_audio_features(artist_name) |>
+    dplyr::filter(album_name == album_name) |>
+    dplyr::select(artist_name, album_images, album_release_date, danceability, energy, valence,
+                  instrumentalness, acousticness, track_name, album_name)
   x <- new_music_visualizer(x)
   x <- validate_music_visualizer(x)
   x
@@ -79,17 +93,22 @@ validate_music_visualizer <- function(x) {
 
 #' Visualize the relevant features - danceability, energy, valence, instrumentalness, and acousticness
 #'
+#' Description HERE!!! My brain is not FUNCTIONING at the moment!!! We will come back in one day!!!
+#'
 #' @param x An ['musicvisualizer'] object
 #' @param ... Currently ignored
 #'
-
+#' @importFrom tidyr pivot_longer
+#' @importFrom ggplot2 ggplot
+#'
+#' @exportS3Method
 plot.musicvisualizer <- function(x, ...) {
 
   tracks_features <- x %>%
-    pivot_longer(cols = c(danceability, energy, valence, acousticness),
-                 names_to = "Features", values_to = "Value of Features")
+    tidyr::pivot_longer(cols = c(danceability, energy, valence, acousticness),
+                        names_to = "Features", values_to = "Value of Features")
 
-  ggplot(tracks_features, aes(x = track_name, y = `Value of Features`, color = Features)) +
+  ggplot2::ggplot(tracks_features, aes(x = track_name, y = `Value of Features`, color = Features)) +
     geom_point(size = 2) +
     theme_minimal() +
     theme(axis.text.x = element_text(angle = 25, hjust = 1)) +
