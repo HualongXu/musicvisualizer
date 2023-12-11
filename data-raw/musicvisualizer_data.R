2820d2d8931c3db9e5a300390175858072d332b1
Hualong Xu <hxu@smith.edu>
2023-12-11 03:03:09 -0500

    required packages added to DESCRIPTION and LICENSE updated

--- data-raw/musicvisualizer_data.R
+++ data-raw/musicvisualizer_data.R
@@ -1,9 +1,6 @@
 ## code to prepare `musicvisualizer` dataset goes here
 
-library(dplyr)
 library(spotifyr)
-library(plotly)
-library(ggplot2)
 
 id <- 'd0f427dc24ea4235a017135aaa1cd5ba'
 secret <- '4866b1333932467cb761e1abd0d669bf'
